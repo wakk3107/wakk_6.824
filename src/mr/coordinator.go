5a6195abdc03f2a36df1608d2c6537170b36cce7
wakk3107 <chenyulin3107@163.com>
2022-09-21 19:36:17 +0800

    lab1 finished

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,15 +1,130 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
 
+var mu sync.Mutex
+
+//type of job
+type JobType int
+
+const (
+	MapJob = iota
+	ReduceJob
+	WaittingJob
+	KillJob
+)
+
+//condition of coordinator
+type Condition int
+
+const (
+	MapPhase = iota
+	ReducePhase
+	AllDone
+)
+
+//condition of job
+type JobCondition int
+
+const (
+	JobWorking = iota
+	JobWaiting
+	JobDone
+)
+
+type Job struct {
+	JobType    JobType
+	InputFile  []string
+	JobId      int
+	ReducerNum int
+	//TmpFileList []string
+}
+
+type JobMetaInfo struct {
+	condition JobCondition
+	StartTime time.Time
+	JobPtr    *Job
+}
 
 type Coordinator struct {
 	// Your definitions here.
+	JobChannelMap        chan *Job
+	JobChannelReduce     chan *Job
+	ReducerNum           int
+	MapNum               int
+	CoordinatorCondition Condition
+	uniqueJobId          int
+	jobMetaHolder        JobMetaHolder
+}
+type JobMetaHolder struct {
+	MetaMap map[int]*JobMetaInfo
+}
 
+func (j *JobMetaHolder) getJobMetaInfo(jobId int) (bool, *JobMetaInfo) {
+	res, ok := j.MetaMap[jobId]
+	return ok, res
+}
+
+func (j *JobMetaHolder) fireTheJob(jobId int) bool {
+	ok, jobInfo := j.getJobMetaInfo(jobId)
+	if !ok || jobInfo.condition != JobWaiting {
+		return false
+	}
+	jobInfo.condition = JobWorking
+	jobInfo.StartTime = time.Now()
+	return true
+}
+func (j *JobMetaHolder) putJob(JobInfo *JobMetaInfo) bool {
+	jobId := JobInfo.JobPtr.JobId
+	meta, _ := j.MetaMap[jobId]
+	if meta != nil {
+		fmt.Println("meta contains job which id = ", jobId)
+		return false
+	} else {
+		j.MetaMap[jobId] = JobInfo
+	}
+	return true
+}
+func (j *JobMetaHolder) checkJobDone() bool {
+	reduceDoneNum := 0
+	reduceUndoneNum := 0
+	mapDoneNum := 0
+	mapUndoneNum := 0
+	for _, v := range j.MetaMap {
+		if v.JobPtr.JobType == MapJob {
+			if v.condition == JobDone {
+				mapDoneNum += 1
+			} else {
+				mapUndoneNum++
+			}
+		} else {
+			if v.condition == JobDone {
+				reduceDoneNum++
+			} else {
+				reduceUndoneNum++
+			}
+		}
+	}
+	fmt.Printf("%d/%d map jobs are done, %d/%d reduce job are done\n",
+		mapDoneNum, mapDoneNum+mapUndoneNum, reduceDoneNum, reduceDoneNum+reduceUndoneNum)
+
+	if (reduceDoneNum > 0 && reduceUndoneNum == 0) || (mapDoneNum > 0 && mapUndoneNum == 0) {
+		return true
+	}
+
+	return false
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -23,7 +138,6 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -46,12 +160,86 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
-	ret := false
+	mu.Lock()
+	defer mu.Unlock()
+	fmt.Println("+++++++++++++++++++++++++++++++++++++++++++")
+	return c.CoordinatorCondition == AllDone
+}
+func (c *Coordinator) JobIsDone(args *Job, reply *ExampleReply) error {
+	mu.Lock()
+	defer mu.Unlock()
+	switch args.JobType {
+	case MapJob:
+		ok, meta := c.jobMetaHolder.getJobMetaInfo(args.JobId)
+		//prevent a duplicated work which returned from another worker
+		if ok && meta.condition == JobWorking {
+			meta.condition = JobDone
+			fmt.Printf("Map task on %d complete\n", args.JobId)
+		} else {
+			fmt.Println("[duplicated] job done", args.JobId)
+		}
+		break
+	case ReduceJob:
+		fmt.Printf("Reduce task on %d complete\n", args.JobId)
+		ok, meta := c.jobMetaHolder.getJobMetaInfo(args.JobId)
+		//prevent a duplicated work which returned from another worker
+		if ok && meta.condition == JobWorking {
+			meta.condition = JobDone
+		} else {
+			fmt.Println("[duplicated] job done", args.JobId)
+		}
+		break
+	default:
+		panic("wrong job done")
+	}
+	return nil
 
-	// Your code here.
+}
+func (c *Coordinator) DistributeJob(args *ExampleArgs, reply *Job) error {
+	mu.Lock()
+	defer mu.Unlock()
+	fmt.Println("coordinator get a request from worker :")
+	if c.CoordinatorCondition == MapPhase {
+		if len(c.JobChannelMap) > 0 {
+			*reply = *<-c.JobChannelMap
+			if !c.jobMetaHolder.fireTheJob(reply.JobId) {
+				fmt.Printf("[duplicated job id]job %d is running\n", reply.JobId)
+			}
+		} else {
+			reply.JobType = WaittingJob
+			if c.jobMetaHolder.checkJobDone() {
+				c.nextPhase()
+			}
+			return nil
+		}
+	} else if c.CoordinatorCondition == ReducePhase {
+		if len(c.JobChannelReduce) > 0 {
+			*reply = *<-c.JobChannelReduce
+			if !c.jobMetaHolder.fireTheJob(reply.JobId) {
+				fmt.Printf("job %d is running\n", reply.JobId)
+			}
+		} else {
+			reply.JobType = WaittingJob
+			if c.jobMetaHolder.checkJobDone() {
+				c.nextPhase()
+			}
+			return nil
+		}
+	} else {
+		reply.JobType = KillJob
+	}
+	return nil
 
-
-	return ret
+}
+func (c *Coordinator) nextPhase() {
+	if c.CoordinatorCondition == MapPhase {
+		//close(c.JobChannelMap)
+		c.makeReduceJobs()
+		c.CoordinatorCondition = ReducePhase
+	} else if c.CoordinatorCondition == ReducePhase {
+		//close(c.JobChannelReduce)
+		c.CoordinatorCondition = AllDone
+	}
 }
 
 //
@@ -60,11 +248,114 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
+	c := Coordinator{
+		JobChannelMap:    make(chan *Job, len(files)),
+		JobChannelReduce: make(chan *Job, nReduce),
+		jobMetaHolder: JobMetaHolder{
+			MetaMap: make(map[int]*JobMetaInfo, len(files)+nReduce),
+		},
+		CoordinatorCondition: MapPhase,
+		ReducerNum:           nReduce,
+		MapNum:               len(files),
+		uniqueJobId:          0,
+	}
+	c.makeMapJobs(files)
 	// Your code here.
-
-
 	c.server()
+	go c.CrashHandler()
 	return &c
+}
+func (c *Coordinator) CrashHandler() {
+	for {
+		time.Sleep(time.Second * 2)
+		mu.Lock()
+		if c.CoordinatorCondition == AllDone {
+			mu.Unlock()
+			return
+		}
+
+		timenow := time.Now()
+		for _, v := range c.jobMetaHolder.MetaMap {
+			fmt.Println(v)
+			if v.condition == JobWorking {
+				fmt.Println("job", v.JobPtr.JobId, " working for ", timenow.Sub(v.StartTime))
+			}
+			if v.condition == JobWorking && time.Now().Sub(v.StartTime) > 8*time.Second {
+				fmt.Println("detect a crash on job ", v.JobPtr.JobId)
+				switch v.JobPtr.JobType {
+				case MapJob:
+					c.JobChannelMap <- v.JobPtr
+					v.condition = JobWaiting
+				case ReduceJob:
+					c.JobChannelReduce <- v.JobPtr
+					v.condition = JobWaiting
+
+				}
+			}
+		}
+		mu.Unlock()
+	}
+
+}
+func (c *Coordinator) generateJobId() int {
+	res := c.uniqueJobId
+	c.uniqueJobId++
+	return res
+}
+
+//before serve
+func (c *Coordinator) makeMapJobs(files []string) {
+	for _, v := range files {
+		id := c.generateJobId()
+		//fmt.Println("making map job :", id)
+		job := Job{
+			JobType:    MapJob,
+			InputFile:  []string{v},
+			JobId:      id,
+			ReducerNum: c.ReducerNum,
+		}
+
+		jobMetaINfo := JobMetaInfo{
+			condition: JobWaiting,
+			JobPtr:    &job,
+		}
+		c.jobMetaHolder.putJob(&jobMetaINfo)
+		fmt.Println("making map job :", &job)
+		c.JobChannelMap <- &job
+	}
+	//defer close(c.JobChannelMap)
+	fmt.Println("done making map jobs")
+	c.jobMetaHolder.checkJobDone()
+}
+func (c *Coordinator) makeReduceJobs() {
+	for i := 0; i < c.ReducerNum; i++ {
+		id := c.generateJobId()
+		fmt.Println("making reduce job :", id)
+		JobToDo := Job{
+			JobType:   ReduceJob,
+			JobId:     id,
+			InputFile: TmpFileAssignHelper(i, "main/mr-tmp"),
+		}
+		jobMetaInfo := JobMetaInfo{
+			condition: JobWaiting,
+			JobPtr:    &JobToDo,
+		}
+		c.jobMetaHolder.putJob(&jobMetaInfo)
+		c.JobChannelReduce <- &JobToDo
+
+	}
+	//defer close(c.JobChannelReduce)
+	fmt.Println("done making reduce jobs")
+	c.jobMetaHolder.checkJobDone()
+}
+func TmpFileAssignHelper(whichReduce int, tmpFileDirectoryName string) []string {
+	var res []string
+	path, _ := os.Getwd()
+	rd, _ := ioutil.ReadDir(path)
+	for _, fi := range rd {
+		if strings.HasPrefix(fi.Name(), "mr-tmp") && strings.HasSuffix(fi.Name(), strconv.Itoa(whichReduce)) {
+			res = append(res, fi.Name())
+		}
+	}
+	return res
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,17 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -13,6 +20,11 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+type ByKey []KeyValue
+
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -24,18 +36,143 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+var WorkId uint
 
 //
 // main/mrworker.go calls this function.
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
+	alive := true
+	attempt := 0
+	for alive {
+		attempt++
+		job := CallJob()
+		switch job.JobType {
+		case MapJob:
+			{
+				DoMap(mapf, job)
+				fmt.Println("do map", job.JobId)
+				JobIsDone(job)
+			}
+		case ReduceJob:
+			if job.JobId >= 8 {
+				DoReduce(reducef, job)
+				fmt.Println("do reduce", job.JobId)
+				JobIsDone(job)
+			}
 
-	// Your worker implementation here.
+		case WaittingJob:
+			fmt.Println("get waiting")
+			time.Sleep(2 * time.Second)
+		case KillJob:
+			{
+				time.Sleep(time.Second)
+				fmt.Println("[Status] :", "a worker terminated........")
+				alive = false
+			}
+		}
+		time.Sleep(time.Second)
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	}
 
+}
+func JobIsDone(job *Job) {
+	args := job
+	reply := &ExampleReply{}
+	call("Coordinator.JobIsDone", &args, &reply)
+}
+func CallJob() *Job {
+	// declare an argument structure.
+	args := ExampleArgs{}
+
+	// fill in the argument(s).
+	args.X = 99
+
+	// declare a reply structure.
+	reply := Job{}
+
+	// send the RPC request, wait for the reply.
+	call("Coordinator.DistributeJob", &args, &reply)
+
+	return &reply
+}
+func DoMap(mapf func(string, string) []KeyValue, response *Job) {
+	var intermediate []KeyValue
+	filename := response.InputFile[0]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	intermediate = mapf(filename, string(content))
+
+	//initialize and loop over []KeyValue
+	rn := response.ReducerNum
+	HashedKV := make([][]KeyValue, rn)
+
+	for _, kv := range intermediate {
+		HashedKV[ihash(kv.Key)%rn] = append(HashedKV[ihash(kv.Key)%rn], kv)
+	}
+	for i := 0; i < rn; i++ {
+		oname := "mr-tmp-" + strconv.Itoa(response.JobId) + "-" + strconv.Itoa(i)
+		ofile, _ := os.Create(oname)
+		enc := json.NewEncoder(ofile)
+		for _, kv := range HashedKV[i] {
+			enc.Encode(kv)
+		}
+		ofile.Close()
+	}
+
+}
+func DoReduce(reducef func(string, []string) string, response *Job) {
+	reduceFileNum := response.JobId
+	intermediate := readFromLocalFile(response.InputFile)
+	sort.Sort(ByKey(intermediate))
+	dir, _ := os.Getwd()
+	//tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	if err != nil {
+		log.Fatal("Failed to create temp file", err)
+	}
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+		fmt.Fprintf(tempFile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	tempFile.Close()
+	oname := fmt.Sprintf("mr-out-%d", reduceFileNum)
+	os.Rename(tempFile.Name(), oname)
+}
+func readFromLocalFile(files []string) []KeyValue {
+	var kva []KeyValue
+	for _, filepath := range files {
+		file, _ := os.Open(filepath)
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kva = append(kva, kv)
+		}
+		file.Close()
+	}
+	return kva
 }
 
 //
@@ -67,9 +204,9 @@ func CallExample() {
 // returns false if something goes wrong.
 //
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
 	c, err := rpc.DialHTTP("unix", sockname)
+	//c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
